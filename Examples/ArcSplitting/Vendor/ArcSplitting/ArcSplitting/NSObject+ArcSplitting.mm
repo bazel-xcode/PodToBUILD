044927ea443a33792ec7d7f8bdebdd48f5cb194b
Jerry Marino <i@jerrymarino.com>
2020-07-16 20:23:41 -0700

    Improve fobjc-arc C++ source file logic
    This PR removes a hack that prevents C++ source files from ending up in
    `non_arc_srcs`. There's a few podspecs like Yoga, and
    FBAllocationTracker that exibit globs like this.
    
    In the integration tests, I've also added asserts using the
    clang `__has_feature` to validate this.
    
    Fixes #67

--- Examples/ArcSplitting/Vendor/ArcSplitting/ArcSplitting/NSObject+ArcSplitting.mm
+++ Examples/ArcSplitting/Vendor/ArcSplitting/ArcSplitting/NSObject+ArcSplitting.mm
@@ -1,4 +1,4 @@
 #if __has_feature(objc_arc)
-#error This file must be compiled with MRR. Use -fobjc-no-arc flag.
+#error This file must be compiled with out arc
 #endif
 
--- Examples/ArcSplitting/Vendor/ArcSplitting/ArcSplitting/Subdir/ArcSplittingSub.m
+++ Examples/ArcSplitting/Vendor/ArcSplitting/ArcSplitting/Subdir/ArcSplittingSub.m
@@ -1,1 +1,3 @@
-
+#if ! __has_feature(objc_arc)
+#error This file must be compiled with arc
+#endif
