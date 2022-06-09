2ec613f4a87dab1f2ea76f30649abdf9458452bf
Liang Ma <lma@pinterest.com>
2022-06-09 01:57:23 -0700

    revert unintentional Examples/FBSDK changes

--- Examples/FBSDK/podfile_deps.py
+++ Examples/FBSDK/podfile_deps.py
@@ -1,16 +1,18 @@
 new_pod_repository(
   name = "FBSDKCoreKit",
-  url = "https://github.com/facebook/facebook-ios-sdk/archive/v9.0.1.zip",
-  generate_header_map = 1,
-  trace = True
+  url = "https://github.com/facebook/facebook-ios-sdk/archive/v7.1.1.zip",
+  podspec_url = "Vendor/Podspecs/FBSDKCoreKit.podspec.json",
+  generate_header_map = 1
 )
 new_pod_repository(
   name = "FBSDKLoginKit",
-  url = "https://github.com/facebook/facebook-ios-sdk/archive/v9.0.1.zip",
+  url = "https://github.com/facebook/facebook-ios-sdk/archive/v7.1.1.zip",
+  podspec_url = "Vendor/Podspecs/FBSDKLoginKit.podspec.json",
   generate_header_map = 1
 )
 new_pod_repository(
   name = "FBSDKShareKit",
-  url = "https://github.com/facebook/facebook-ios-sdk/archive/v9.0.1.zip",
+  url = "https://github.com/facebook/facebook-ios-sdk/archive/v7.1.1.zip",
+  podspec_url = "Vendor/Podspecs/FBSDKShareKit.podspec.json",
   generate_header_map = 1
 )
